b66aee1ebc9e78f9f6b396126a6fe9d213720e63
NaofumiOkamoto <n.okamooo@gmail.com>
2019-07-09 14:17:51 +0900

    [Finish] 4-8課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
       book = Book.new(book_params)
       book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(book.id)
   end
 
@@ -20,6 +21,7 @@ class BooksController < ApplicationController
   def update
       @book = Book.find(params[:id])
       @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
       redirect_to book_path(@book.id)
   end
 
